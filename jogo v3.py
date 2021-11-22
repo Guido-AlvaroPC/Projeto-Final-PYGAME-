c6cd3c63e6a9e9f5c64b86666af66d9311c3111a
Guido-AlvaroPC <guidohd1000@gmail.com>
2021-11-22 10:44:34 -0300

    jogo v3.2

--- jogo v3.py
+++ jogo v3.py
@@ -14,7 +14,7 @@ pygame.display.set_caption('Teste')
 # ----- Inicia estruturas de dados
 game = True
 lista_palavras = ['Bola', 'Areia', 'Sol', 'Água', 'Vento', 'Churrasco', 'Carangueijo']
-palavras = random.choice(lista_palavras)
+#palavras = random.choice(lista_palavras)
 palavra_x = random.randint(0,500)
 palavra_y = random.randint(-100,0)
 palavra_velocidadex = random.randint(-3,3)
@@ -22,7 +22,11 @@ palavra_velocidadey = random.randint(2,9)
 clock = pygame.time.Clock()
 FPS = 60
 
-#class PALAVRA(pygame.sprite.Sprite):
+class PALAVRA(pygame.sprite.Sprite):
+    def __init__(self,palavra):
+        pygame.sprite.Sprite.__init__(self,img)
+        
+
 
         
 
@@ -36,11 +40,14 @@ musica = pygame.mixer.music.load('assets/snd/theme.mp3')
 volume = pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
 
+todas_palavras = pygame.sprite.Group()
+todas_sprites = pygame.sprite.Group()
+
+
 # ===== Loop principal =====
 while game:
     clock.tick(FPS)
-    texto = palavras
-    txt_tela = fonte.render(texto, True,(0,0,0))
+
 
     # ----- Trata eventos
 
@@ -49,15 +56,19 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
-    palavra_x += palavra_velocidadex
-    palavra_y += palavra_velocidadey
 
+    while len(todas_palavras) < 10:
+        palavra = PALAVRA(random.choice(lista_palavras))
+        todas_palavras.add(palavra)
+        todas_sprites.add(palavra)
+
+    todas_sprites.update()
 
 
     # ----- Gera saídas
     #window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(fundo, (0, 0))
-    window.blit(txt_tela, (palavra_x,palavra_y))
+    
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
