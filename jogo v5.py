21fdf9c86f14310d06c58a3d73b8502cfcd610e9
Guido-AlvaroPC <guidohd1000@gmail.com>
2021-11-30 22:09:44 -0300

    cenarios

--- jogo v5.py
+++ jogo v5.py
@@ -9,9 +9,9 @@ screen = pygame.display.set_mode((width,height))
 def gera_palavra():
     global words
     wordStr = random.choice(words).strip()
-    return TypingGameWord(wordStr)
+    return DestroiPalavras(wordStr)
 
-class TypingGameWord(pygame.sprite.Sprite):
+class DestroiPalavras(pygame.sprite.Sprite):
     def __init__(self, word):
         global width
         pygame.sprite.Sprite.__init__(self)
@@ -29,3 +29,43 @@ class TypingGameWord(pygame.sprite.Sprite):
             self.word = self.word[1:]
             self.atualiza_imagem()
         return self.word == ""
+
+    def atualiza_imagem(self):
+        self.image = self.font.render(self.word, True, (255,0,0))
+        right = self.rect.right
+        bottom = self.rect.bottom
+        self.rect = self.image.get_rect()
+        self.rect.right = right
+        self.rect.bottom = bottom
+
+    def atualiza(self):
+        global height
+        global speed, running, score, extra_words, currentword
+        speedC = len(self.originalWord)
+        if speedC < len(currentword.originalWord):
+            speedC = len(currentword.originalWord)
+        if speedC < 5:
+            speedC = 5
+        old_top = self.rect.top
+        self.rect.top += speed / speedC
+        if old_top < height/4 and self.rect.top >= height/4:
+            extra_words.append(gera_palavra())
+        if self.rect.bottom >= height:
+            lose()
+    def lose():
+            print ("GAME OVER!"),score
+            running = False
+
+running = True
+speed = 10
+wordfile = open('words.txt', 'r')
+words = wordfile.readlines()
+currentword = gera_palavra()
+extra_words = []
+wordfile.close()
+score = 0
+
+score_font = pygame.font.Font("assets/font/score_font.ttf",60)
+background = pygame.image.load("assets/img/praia.png").convert()
+
+#loop principal
